31841fd23fb5796f153c35fb15c74ac947182cfa
Omelet <522981616@qq.com>
2017-11-09 14:27:47 +0800

    [component] radio

--- src/packages/radio/radio.vue
+++ src/packages/radio/radio.vue
@@ -2,23 +2,30 @@
   <label :class="[
     'om-radio',
     { 'is-disabled' : !!disabled },
-    { 'is-checked' : !!checked }
-  ]">
+    { 'is-checked' : checked }]"
+    role="radio"
+    :aria-checked="model === label"
+    :aria-disabled="disabled"
+    @keydown.space.stop.prevent="model = label"
+  >
     <span :class="[
       'om-radio__input',
       { 'is-disabled' : !!disabled },
-      { 'is-checked' : !!checked }
+      { 'is-checked' : checked }
     ]">
       <span class="om-radio__inner"></span>
       <input class="om-radio__origin"
         type="radio"
+        :value="label"
+        :name="name"
+        v-model="model"
         tabindex="-1"
         :disabled="disabled"
-        :checked="checked"
-        :v-model="value">
+        @change="handleChange">
     </span>
     <span class="om-radio__label">
       <slot></slot>
+      <template v-if="!$slots.default">{{ label }}</template>
     </span>
   </label>
 </template>
@@ -26,6 +33,8 @@
 <script>
 export default {
   name: 'OmRadio',
+
+  componentsName: 'OmRadio',
 
   props: {
     label: [ String, Number, Boolean, Object ],
@@ -47,14 +56,44 @@ export default {
     name: String
   },
 
+  data() {
+    return {
+      _radioGroup: null
+    }
+  },
+
   computed: {
+    isGroup() {
+      let parent = this.$parent
+      while (parent) {
+        if (parent.$options.componentsName !== 'OmRadioGroup') {
+          parent = parent.$parent
+        } else {
+          this._radioGroup = parent
+          return true
+        }
+      }
+      return false
+    },
+    model: {
+      get() {
+        return this._radioGroup ? this._radioGroup.value : this.value
+      },
+      set(val) {
+        this.$emit('input', val)
+      }
+    },
     checked() {
-      return this.value === this.label
+      return this.label === this.model
+    }
+  },
+
+  methods: {
+    handleChange() {
+      // this.$nextTick(() => {
+      //   this.$emit('change', this.model)
+      // })
     }
   }
 }
 </script>
-
-<style>
-
-</style>
